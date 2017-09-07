87a2d5382218058780f13263728109a8ab8c137e
Roberto Ughi <umbe77@gmail.com>
2017-09-08 00:17:14 +0200

    Added Middleware supports

---
+++ test/test-middleware.js
@@ -0,0 +1,26 @@
+const {rest, router} = require('../index')
+
+rest.createRest({
+    port: 8088
+})
+
+rest.addMiddleware((svc) => {
+    svc.context.helloMessage = "ciao"
+})
+
+rest.addMiddleware((svc) => {
+    svc.context.helloMessage = "ciao second"
+    svc.context.name = "Roberto"
+})
+
+
+router.get("/", (svc) => {
+    svc.done({
+        body: `context: ${svc.context.helloMessage} -- ${svc.context.name}`,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+})
+
+rest.run()
